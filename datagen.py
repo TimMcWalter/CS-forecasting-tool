0f30ce2af4e7e65cbd20667ddf6f5a14da59ea30
TimMcWalter <timmcwalter@gmail.com>
2026-04-30 19:00:29 +0200

    datagen

---
+++ datagen.py
@@ -0,0 +1,26 @@
+import pandas as pd, numpy as np
+
+def gen_demand(numberOfSKU,startdate,enddate):
+    """ aggregate the dates, get the holidays, special days etc
+        fix hard coded factors, 
+        add noise through randomness
+        add all factors to gen demand for each SKU
+        website traffic should be a similar to website traffic
+    """
+
+    gen_company()
+    pass
+
+
+
+
+def gen_company(startdate,enddate):
+    pass
+
+def datagen(numberOfSKU,startdate,enddate):
+    df= pd.DataFrame()
+    gen_demand()
+    
+    
+
+
